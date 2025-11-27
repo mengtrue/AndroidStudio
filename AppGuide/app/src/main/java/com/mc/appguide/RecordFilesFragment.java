325218eb4dc43d069cc2f447b4762fa7983cd92d
mengtrue <jmc313@163.com>
2025-11-27 22:38:21 +0800

    update action mode

--- AppGuide/app/src/main/java/com/mc/appguide/RecordFilesFragment.java
+++ AppGuide/app/src/main/java/com/mc/appguide/RecordFilesFragment.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -34,6 +33,33 @@ public class RecordFilesFragment extends Fragment {
     private ActionMode mActionMode;
     private MediaPlayer mMediaPlayer;
 
+    private AbsListView.MultiChoiceModeListener mMultiChoiceModeListener = new AbsListView.MultiChoiceModeListener() {
+        @Override
+        public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+            handleMultiChoiceModeItemChecked(position, checked);
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return handleMultiChoiceModeActionClicked(item);
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            return handleMultiChoiceModeActionModeCreated(mode, menu);
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            handleMultiChoiceModeActionDestroyed();
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+    };
+
     @SuppressWarnings("deprecation")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +69,7 @@ public class RecordFilesFragment extends Fragment {
 
         mMediaPlayer = new MediaPlayer();
         loadRecordFiles();
+
         mAdapter = new RecordListAdapter();
         mRecordListView.setAdapter(mAdapter);
         setupRecordListViewListeners();
@@ -59,73 +86,43 @@ public class RecordFilesFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        if (mActionMode != null) {
+            mActionMode.finish();
+            mActionMode = null;
+        }
         if (mMediaPlayer != null) {
             if (mMediaPlayer.isPlaying()) mMediaPlayer.stop();
             mMediaPlayer.release();
             mMediaPlayer = null;
         }
+        mRecordFiles.clear();
     }
 
     private void setupRecordListViewListeners() {
         mRecordListView.setOnItemClickListener((parent, view, position, id) -> {
             Log.d(TAG, "OnItemClick selection mode : " + mIsSelectionMode);
+            if (position < 0 || position >= mRecordFiles.size()) return;
             RecordFile recordFile = mRecordFiles.get(position);
-            if (recordFile.isPlaying()) {
-                pauseRecordFile();
+            if (mIsSelectionMode) {
+                recordFile.setSelected(!recordFile.isSelected());
+                mAdapter.notifyDataSetChanged();
+                updateActionModeTitle();
+                mRecordListView.setItemChecked(position, recordFile.isSelected());
             } else {
-                if (mCurrentPlayingPosition != -1) {
-                    mRecordFiles.get(mCurrentPlayingPosition).setPlaying(false);
-                    mAdapter.notifyDataSetChanged();
+                if (recordFile.isPlaying()) {
+                    pauseRecordFile();
+                } else {
+                    if (mCurrentPlayingPosition != -1 && mCurrentPlayingPosition < mRecordFiles.size()) {
+                        mRecordFiles.get(mCurrentPlayingPosition).setPlaying(false);
+                    }
+                    playRecordFile(position, recordFile.getFilePath());
                 }
-                playRecordFile(position, recordFile.getFilePath());
+                mAdapter.notifyDataSetChanged();
             }
         });
         mRecordListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        mRecordListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
-            @Override
-            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-                Log.d(TAG, "onItemChecked selection mode : " + mIsSelectionMode);
-                mRecordFiles.get(position).setSelected(checked);
-                mAdapter.notifyDataSetChanged();
-                updateActionModeTitle();
-            }
-
-            @Override
-            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-                if (item.getItemId() == R.id.action_delete) {
-                    deleteSelectedFiles();
-                    mode.finish();
-                    return true;
-                } else if (item.getItemId() == R.id.action_select_all) {
-                    updateSelectAllAction();
-                    return true;
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-                Log.d(TAG, "oncreate action mode");
-                mode.getMenuInflater().inflate(R.menu.menu_record_selection, menu);
-                mIsSelectionMode = true;
-                mActionMode = mode;
-                updateActionModeTitle();
-                return true;
-            }
-
-            @Override
-            public void onDestroyActionMode(ActionMode mode) {
-                mIsSelectionMode = false;
-                mActionMode = null;
-                clearSelection();
-                mAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-                return false;
-            }
-        });
+        mRecordListView.setMultiChoiceModeListener(mMultiChoiceModeListener);
     }
 
     private void loadRecordFiles() {
@@ -144,6 +141,50 @@ public class RecordFilesFragment extends Fragment {
             int count = getSelectedCount();
             mActionMode.setTitle(count + " 已选择");
         }
+    }
+
+    private void handleMultiChoiceModeItemChecked(int position, boolean checked) {
+        if (position >= 0 && position < mRecordFiles.size()) {
+            mRecordFiles.get(position).setSelected(checked);
+            mAdapter.notifyDataSetChanged();
+            updateActionModeTitle();
+        }
+    }
+
+    private boolean handleMultiChoiceModeActionModeCreated(ActionMode mode, Menu menu) {
+        Log.d(TAG, "handleMultiChoiceModeActionModeCreated");
+        try {
+            mode.getMenuInflater().inflate(R.menu.menu_record_selection, menu);
+            mIsSelectionMode = true;
+            mActionMode = mode;
+            updateActionModeTitle();
+            mAdapter.notifyDataSetChanged();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean handleMultiChoiceModeActionClicked(MenuItem item) {
+        if (item.getItemId() == R.id.action_delete) {
+            deleteSelectedFiles();
+            return true;
+        } else if (item.getItemId() == R.id.action_select_all) {
+            updateSelectAllAction();
+            return true;
+        }
+        return false;
+    }
+
+    private void handleMultiChoiceModeActionDestroyed() {
+        Log.d(TAG, "handleMultiChoiceModeActionDestroyed, selection : " + mIsSelectionMode + ", actionMode : " + mActionMode);
+        if (!mIsSelectionMode || mActionMode == null) return;
+        mIsSelectionMode = false;
+        mActionMode = null;
+        clearSelection();
+        mRecordListView.invalidate();
+        mAdapter.notifyDataSetChanged();
     }
 
     private int getSelectedCount() {
@@ -176,16 +217,24 @@ public class RecordFilesFragment extends Fragment {
     }
 
     private void deleteSelectedFiles() {
+        Log.d(TAG, "deleteSelectedFiles");
         List<RecordFile> removedFiles = new ArrayList<>();
         for (RecordFile file : mRecordFiles) {
             if (file.isSelected()) removedFiles.add(file);
         }
         if (mCurrentPlayingPosition != -1 && mRecordFiles.get(mCurrentPlayingPosition).isSelected()) {
-            if (mMediaPlayer.isPlaying()) mMediaPlayer.stop();
+            if (mMediaPlayer != null && mMediaPlayer.isPlaying()) mMediaPlayer.stop();
             mCurrentPlayingPosition = -1;
         }
         mRecordFiles.removeAll(removedFiles);
+        resetRecordListView();
         mAdapter.notifyDataSetChanged();
+        if (mActionMode != null) mActionMode.finish();
+    }
+
+    private void resetRecordListView() {
+        clearSelection();
+        if (mRecordFiles.isEmpty()) mRecordListView.setSelection(0);
     }
 
     private void playRecordFile(int position, String filePath) {
@@ -262,7 +311,7 @@ public class RecordFilesFragment extends Fragment {
         //Get a View that displays the data at the specified position in the data set
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            Log.d(TAG, "getview selectionmode : " + mIsSelectionMode);
+            Log.d(TAG, "getview position : " + position + ", selectionmode : " + mIsSelectionMode);
             RecordFileViewHolder holder;
             if (convertView == null) {
                 convertView = mLayoutInflater.inflate(R.layout.record_list_item, parent, false);
@@ -270,7 +319,6 @@ public class RecordFilesFragment extends Fragment {
                 holder.tv_title = convertView.findViewById(R.id.record_title);
                 holder.tv_duration = convertView.findViewById(R.id.record_duration);
                 holder.cb_delete = convertView.findViewById(R.id.cb_delete);
-                holder.fileView = convertView.findViewById(R.id.record_file_view);
                 convertView.setTag(holder);
             } else {
                 holder = (RecordFileViewHolder) convertView.getTag();
@@ -284,11 +332,6 @@ public class RecordFilesFragment extends Fragment {
                 holder.cb_delete.setChecked(recordFile.isSelected());
                 holder.cb_delete.setOnCheckedChangeListener((buttonView, isChecked) -> {
                     recordFile.setSelected(isChecked);
-                    mAdapter.notifyDataSetChanged();
-                    updateActionModeTitle();
-                });
-                holder.fileView.setOnClickListener(v -> {
-                    recordFile.setSelected(!recordFile.isSelected());
                     mAdapter.notifyDataSetChanged();
                     updateActionModeTitle();
                 });
@@ -310,6 +353,5 @@ public class RecordFilesFragment extends Fragment {
         TextView tv_title;
         TextView tv_duration;
         CheckBox cb_delete;
-        LinearLayout fileView;
     }
 }
